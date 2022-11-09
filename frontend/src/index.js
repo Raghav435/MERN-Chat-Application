3f16dca53f502803a3f3e0cc2bbcc071e5cfd46d
Raghav Shukla <shuklaraghav435@gmail.com>
2022-11-09 08:47:32 +0530

    dependency install & Initial pages added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,4 +4,12 @@ import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <Router>
+      <ChatProvider>
+        <App />
+      </ChatProvider>
+    </Router>
+  </ChakraProvider>
+);
